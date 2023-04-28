1d43e7d300ba62d5041f9621b742db2714e4d5dc
Zach Smith <Zachary.Clay.Smith@gmail.com>
2023-04-28 11:51:50 -0700

    removed type:module, changed module from esnext->commonjs, and shifted index.ts to not have top-level await

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 
 import { UserResolver } from "./resolvers/UserResolver";
 import { buildSchema } from "type-graphql";
 
-await buildSchema({
-  resolvers: [UserResolver],
-  // HACK: https://github.com/MichalLytek/type-graphql/issues/1396
-  validate: { forbidUnknownValues: false },
+(async () => {
+  await buildSchema({
+    resolvers: [UserResolver],
+    // HACK: https://github.com/MichalLytek/type-graphql/issues/1396
+    validate: { forbidUnknownValues: false },
+  });
+})().catch((e) => {
+	console.error(e);
 });
